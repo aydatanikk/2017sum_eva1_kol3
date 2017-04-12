1b528a9d81bf3e5f03c3be370c97126bb6ee5960
aydatanikk <atanik95@gmail.com>
2017-04-12 18:54:47 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
+import unittest
+class matrixTests(unittest.TestCase):
 
+
+ def __init__test1(self, methodName='runTest', param=None):
+        super(ParametrizedTestCase, self).__init__(__init__)
+        self.param = param
+
+def  test2(self,other):
+      self.assertEqual(matris. __add__("[1,1,1],[1,1,1],[1,1,1]"),[1,1,1],[1,1,1],[1,1,1])
+      self.assertEqual(matris. __add__("[1,2,3],[1,2,3],[1,2,3]"),[1,2,3],[1,2,3],[1,2,3])
+
+def __str__test(self):
+       super(ParametrizedTestCase, self).__str__(__str__)
+       self.param = param
+
+def __iter__test(self):
+      super(ParametrizedTestCase, self).__iter__(__iter__)
+      self.param = param
